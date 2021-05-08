8093a1a7ab84b69ff6b55302eece30f94344d0c1
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-05-08 09:35:50 +0800

    text file mining

--- TextMining.py
+++ TextMining.py
@@ -1,1 +1,24 @@
-print("hello world")
+def countWords(file):
+    words = {}
+    i = 0
+    for line in file:
+        w = line.split(" ") #output list
+        for b in line.split():
+            words[b] = words.get(b,0) + 1
+
+
+        for w,c in words.items():
+            print("%s: %d times" % (w,c))
+
+
+
+
+
+
+
+
+
+file = open("mytext.txt", "r")
+countWords(file)
+
+
