788fa3365eedbdf94fa9750515ef5901baf515f8
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-05-10 21:31:03 +0800

    word a fixed

--- TextCounter.py
+++ TextCounter.py
@@ -46,7 +46,10 @@ def logic(string):
         words[word] = words.get(word, 0) + 1  # transform file to dict # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
 
     for w, c in words.items():  # print lang pero di to gagawin graph dapat
-        if len(w) > 3:  # if less than 3 letter sya #note:pwede to tanggalin kung ano na lng trip nyo
+        if len(w) > 1 or (len(w) > 0 and w==('a'.lower())): # if less than 3 letter sya #note:pwede to tanggalin kung ano na lng trip nyo
             print("%s: %d times" % (w, c))
+
+
+
 
 counter(filename)
