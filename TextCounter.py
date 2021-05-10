a521035a11f08df8eec219cf6614156c7a74487d
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-05-10 17:07:48 +0800

    pdf and docx

--- TextCounter.py
+++ TextCounter.py
@@ -1,35 +1,52 @@
 import docx2txt
 import re
+import io
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
 
 filename = str(input("Enter file name"))
 def counter(extension):
 
     if extension.__contains__('.docx') or extension.__contains__('.doc'):
+        doc = docx2txt.process(filename)  # extract docx to text
         # open connection to Word Document
-        doc = docx2txt.process(filename)#extract docx to text
+        logic(doc)
+    elif extension.__contains__(".pdf"):
         words = {}
-        remove =" ".join(re.findall(r"[a-zA-Z0-9]+", doc)).lower()# tanggal special character
-        for remove in remove.split():#para kada space isang word
-            words[remove] = words.get(remove, 0) + 1  # transform file to dict # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
+        logic(extract_text(filename))
 
-            for w, c in words.items():#print lang pero di to gagawin graph dapat
-                if len(w) > 3:  # if less than 3 word sya #note:pwede to tanggalin kung ano na lng trip nyo
-                    print("%s: %d times" % (w, c))
+def extract_text_by_page(filename):
+    with open(r""+filename, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            resource_manager = PDFResourceManager()
+            fake_file_handle = io.StringIO()
+            converter = TextConverter(resource_manager, fake_file_handle)
+            page_interpreter = PDFPageInterpreter(resource_manager, converter)
+            page_interpreter.process_page(page)
 
-    elif extension.__contains__(".txt"):
+            text = fake_file_handle.getvalue()
+            yield text
 
-        file = open(filename, "r")
-        words = {}
-
-        for line in file:#para kada space isang word
-            remove = " ".join(re.findall(r"[a-zA-Z0-9]+", line)).lower()# tanggal special character
-            for b in remove.split():
-                words[b] = words.get(b,0) + 1 # transform file to dict  # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
 
 
-            for w, c in words.items():#print lang pero di to gagawin graph dapat
-                if len(w) > 3:# if less than 3 word sya #note:pwede to tanggalin kung ano na lng trip nyo
-                    print("%s: %d times" % (w, c))
+def extract_text(filename):
+    for page in extract_text_by_page(filename):
+        return page
+def logic(string):
 
+    words = {}
+    remove = " ".join(re.findall(r"[a-zA-Z0-9]+", string)).lower()  # tanggal special character
+    for word in remove.split():  # para kada space isang word
+
+        words[word] = words.get(word, 0) + 1  # transform file to dict # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
+
+    for w, c in words.items():  # print lang pero di to gagawin graph dapat
+        if len(w) > 3:  # if less than 3 letter sya #note:pwede to tanggalin kung ano na lng trip nyo
+            print("%s: %d times" % (w, c))
 
 counter(filename)
