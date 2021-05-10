b1954261c37611046166c9801e816a48634444d9
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-05-10 11:06:40 +0800

    a

--- TextCounter.py
+++ TextCounter.py
@@ -6,13 +6,13 @@ def counter(extension):
 
     if extension.__contains__('.docx') or extension.__contains__('.doc'):
         # open connection to Word Document
-        doc = docx2txt.process(filename)
+        doc = docx2txt.process(filename)#extract docx to text
         words = {}
-        remove =" ".join(re.findall(r"[a-zA-Z0-9]+", doc)).lower()
-        for remove in remove.split():
-            words[remove] = words.get(remove, 0) + 1  # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
+        remove =" ".join(re.findall(r"[a-zA-Z0-9]+", doc)).lower()# tanggal special character
+        for remove in remove.split():#para kada space isang word
+            words[remove] = words.get(remove, 0) + 1  # transform file to dict # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
 
-            for w, c in words.items():
+            for w, c in words.items():#print lang pero di to gagawin graph dapat
                 print("%s: %d times" % (w, c))
 
     elif extension.__contains__(".txt"):
@@ -20,12 +20,12 @@ def counter(extension):
         file = open(filename, "r")
         words = {}
 
-        for line in file:
-            remove = " ".join(re.findall(r"[a-zA-Z0-9]+", line)).lower()
-            for b in remove.split():  # transform file to dict
-                words[b] = words.get(b,0) + 1  # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
+        for line in file:#para kada space isang word
+            remove = " ".join(re.findall(r"[a-zA-Z0-9]+", line)).lower()# tanggal special character
+            for b in remove.split():
+                words[b] = words.get(b,0) + 1 # transform file to dict  # dict kasi para makuha yung key example (2:"the") means two time lumabas yung the
 
-            for w, c in words.items():
+            for w, c in words.items():#print lang pero di to gagawin graph dapat
                 print("%s: %d times" % (w, c))
 
 
